c2f0bd7c7b0b395cc5ff7c24ed4b5ad98d9523c7
anshui <aggarwalanshul01@gmail.com>
2020-09-20 15:45:43 +0530

    Heroku first deploy

---
+++ db.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize')
+
+let db;
+
+if (process.env.DATABASE_URL) {
+    db = new Sequelize(process.env.DATABASE_URL)
+} else {
+    db = new Sequelize({
+        dialect: 'sqlite',
+        storage: __dirname + '/test.db'
+    })
+}
+
+const Tasks = db.define('task', {
+    id: {
+        type: Sequelize.DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: {
+        type: Sequelize.DataTypes.STRING,
+        allowNull: false
+    },
+    done: {
+        type: Sequelize.DataTypes.BOOLEAN,
+        defaultValue: false
+    },
+    priority: {
+        type: Sequelize.DataTypes.ENUM('high', 'low', 'normal'),
+        defaultValue: 'normal'
+    }
+})
+
+module.exports = {
+    db,
+    Tasks
+}
