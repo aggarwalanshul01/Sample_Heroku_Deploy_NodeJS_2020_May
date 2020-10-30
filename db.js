fa05b62c85d75c3f67b44d0abe1886a67da3b84d
anshui <aggarwalanshul01@gmail.com>
2020-10-30 20:07:49 +0530

    normal

--- db.js
+++ db.js
@@ -1,15 +1,18 @@
 const Sequelize = require('sequelize')
 
 let db;
-
-if (process.env.DATABASE_URL) {
-    db = new Sequelize(process.env.DATABASE_URL)
-} else {
-    db = new Sequelize({
-        dialect: 'sqlite',
-        storage: __dirname + '/test.db'
+//console.log(process.env.DATABASE_URL);
+// if (process.env.DATABASE_URL) {
+//     db = new Sequelize(process.env.DATABASE_URL)
+// } else {
+db = new Sequelize({
+        dialect: 'postgres',
+        database: 'dbkj0pur22jl0t',
+        username: 'lrmytorrczblnk',
+        password: 'e97275656681a79e50d9676d9bf3f70bca0d8dfcd89970b435b5e76f9644bd00',
+        host: 'ec2-3-208-224-152.compute-1.amazonaws.com'
     })
-}
+    //}
 
 const Tasks = db.define('task', {
     id: {
